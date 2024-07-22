d32e1b8a75ca1621ef5fb92fb0986d9ea22fc24e
Shaqeel Less <54390756+MogammadShaqeelless16@users.noreply.github.com>
2024-07-22 07:32:23 +0000

    Update

--- src/utils/app-settings.ts
+++ src/utils/app-settings.ts
@@ -3,5 +3,5 @@ export const NIGHTLY_LATE_RETURN_PENALTY = 10;
 export const MAXIMUM_TAPE_ALLOWED_TO_RENT = 4;
 export const DEPOSIT = 25;
 
-export const MINIMUM_APP_WIDTH = 1024;
-export const MINIMUM_APP_HEIGHT = 768;
+export const MINIMUM_APP_WIDTH = 375;
+export const MINIMUM_APP_HEIGHT = 667;
